54275562d3b689baed1c72891b114f9c48ce3438
Jorge M Pacheco <jmpacheco1997@gmail.com>
2021-09-01 09:21:55 -0300

    Resolver conflict

--- MergeMe/MergeMe/User.cs
+++ MergeMe/MergeMe/User.cs
@@ -12,11 +12,8 @@ namespace MergeMe
 
         public string LastName { get; set; }
 
-<<<<<<< HEAD
-        public string FullName { get => $"{Name} {LastName}"; } 
-=======
-        public string FullName { get => Name.ToUpper() + " " + LastName.ToUpper(); } 
->>>>>>> origin/uppercase
+
+        public string FullName { get => $"{Name.ToUpper()} {LastName.ToUpper}"; } 
 
     }
 }
